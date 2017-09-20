645fd9539e2e5eb6329330566ca0ef6b0ec83a9d
harushi <sunny.refreshing.lyrics2424@gmail.com>
2017-09-21 01:05:20 +0900

    とりあえずコミット

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by 菅晴詞 on 2017/09/21.
+//  Copyright © 2017年 Haru Su. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
